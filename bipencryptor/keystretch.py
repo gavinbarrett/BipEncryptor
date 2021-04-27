aaa80d140f4a1928bf7cb8f9fd120c8fa9735f60
Gavin <gavinbrrtt@gmail.com>
2021-04-26 21:42:53 -0700

    Added input line to prompt user to hit the enter key before revealing their decrypted mnemonic phrase.

--- bipencryptor/keystretch.py
+++ bipencryptor/keystretch.py
@@ -3,19 +3,16 @@ from os import cpu_count
 from psutil import virtual_memory
 from argon2 import hash_password_raw, low_level
 
-white = '\u001b[37m'
-end = '\033[0m'
-
 def kdf(passphrase):
 	''' Derive the key from a passphrase '''
 	time = 16
 	mem = floor((virtual_memory().free * 0.75)/1000) # use 75% of available memory
 	cores = cpu_count()
-	print(f'{white}Stretching key...{end}')
+	print(f'Stretching key...')
 	return hash_password_raw(time_cost=time, memory_cost=mem, parallelism=cores, hash_len=32, password=passphrase, salt=b"k3y der1vati0n", type=low_level.Type.ID), f'{time};{mem};{cores}'
 
 def kdf_decrypt(passphrase, time, mem, cores):
-	print(f'{white}Stretching key...{end}')
+	print(f'Stretching key...')
 	# compute 50 of the available memory
 	return hash_password_raw(time_cost=int(time), memory_cost=int(mem), parallelism=int(cores), hash_len=32, password=passphrase, salt=b"k3y der1vati0n", type=low_level.Type.ID)
 	
